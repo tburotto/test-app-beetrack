d79713945283138db32c2980a2d56b995dd6a989
Tburotto <tfburotto@uc.cl>
2020-03-21 16:12:50 -0300

    :sparkles: Added API endpoints for gps tracks

---
+++ app/controllers/api/v1/gps_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::GpsController < ApplicationController
+    protect_from_forgery with: :null_session
+
+    def index
+        @tracks = Track.all
+
+        render json: @tracks, status: :ok
+    end
+
+    def create
+        @vehicle = Vehicle.where(identifier: params[:vehicle_identifier]).first
+        if @vehicle == nil
+            @vehicle = Vehicle.create(identifier: params[:vehicle_identifier])
+        end
+        @track = Track.new(latitude: params[:latitude], longitude: params[:longitude], send_at: params[:send_at], vehicle: @vehicle)
+        if @track.save
+            render json: @track, status: :created
+        else
+            head(:unprocessable_entity)
+        end
+        
+    end
+    private
+
+end
